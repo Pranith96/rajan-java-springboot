2f9b8b0e8d84b2c9f1f200a6fb61d2faab4aa20d
Your Name <you@example.com>
2022-03-24 11:21:16 +0530

    spring boot application

---
+++ student-rajan-application/src/main/java/com/student/entity/Student.java
@@ -0,0 +1,77 @@
+package com.student.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student_table")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_id")
+	private Integer studentId;
+	@Column(name = "student_name")
+	private String studentName;
+	@Column(name = "mobile_number")
+	private String mobileNumber;
+	@Column(name = "email_id")
+	private String email;
+	@Column(name = "login_id", unique = true)
+	private String loginId;
+	@Column(name = "password")
+	private String password;
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
